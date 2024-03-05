3461dd6af968c6be6b2486173542318b9a604df8
kaiwright <kaimatthewwright@gmail.com>
2024-03-05 18:52:53 +0000

    Updating homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+
 import Container from '../components/Container'
 import Hero from '../components/Hero';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import projectData from '../data/projects.json';
+import ProjectCard from '../components/ProjectCard';
 
 
 function Home() {
+    const projects = projectData
+
     return (
         <div>
             <Container>
-            <Hero className='w-100'>
-                
-            </Hero>
-            <Row>
-                <Col>
-                <img src='/imgs/Profile_Pic.jpg'  width={'500px'}></img>
-                </Col>
-                <Col>
-                <h3 className='text'>👋 Hi there!</h3>
-                <h4 className='text'>I'm Kai, a digital designer with experience in UI/UX Design and Front-end Web Development. I love combining my creative and technical knowledge to develop applications that are both visually appealing and user-friendly.</h4>
-                </Col>
-                
-            </Row>
-            
+                <Hero/>
+
+                <Container>
+                    {/* Layout of cards */}
+                    <Row xs={1} md={2} lg={3}>
+                        {/* Maps project information onto each card */}
+                        {projects.map((project) => (
+                            <ProjectCard
+                                key={project.id}
+                                imgUrl={project.img}
+                                title={project.name}
+                                description={project.description}
+                                link={project.link}
+                                repo={project.repo}
+                            />
+                        ))}
+                    </Row>
+                </Container>
             </Container>
         </div>
     )
